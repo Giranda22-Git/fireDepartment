e208d8928d91e3e7ef279e3da5af221b668152e5
Heigen007 <67007974+Heigen007@users.noreply.github.com>
2021-04-01 18:52:27 +0600

    init

---
+++ frontEnd/fireapp/src/layouts/LoginLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <LeftDrower />
+</template>
+
+<script>
+import LeftDrower from 'components/LeftDrower.vue'
+
+export default {
+  name: 'LoginLayout',
+  components: { LeftDrower },
+  mounted () {
+  }
+}
+</script>

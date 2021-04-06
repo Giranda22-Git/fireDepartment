e2e9c47797b992ed377127a5de647e1e66bd2caf
Heigen007 <67007974+Heigen007@users.noreply.github.com>
2021-04-06 14:20:43 +0600

    Added pulse animation on searching fire car

--- frontEnd/fireapp/src/components/PulseAnimation.vue
+++ frontEnd/fireapp/src/components/PulseAnimation.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="pulse">
-    <div style="position: absolute; z-index: 10; font-size: 2em; color: #ffb340">
-      Поиск...
-    </div>
     <span style="--i: 1"></span>
     <span style="--i: 2"></span>
     <span style="--i: 3"></span>
-    <span style="--i: 4"></span>
-    <span style="--i: 5"></span>
   </div>
 </template>
 
@@ -25,47 +20,37 @@ export default {
   box-sizing: border-box;
 }
 .pulse {
-  animation: opacity 2s;
   position: absolute;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100px;
-  height: 100px;
-  background: #b43131;
   border-radius: 50%;
-  top: 53vh
+  pointer-events: none;
 }
 .pulse span {
   position: absolute;
-  top: 0;
-  left: 0;
+  top: 52%;
   background-color: #b43131;
   border-radius: 50%;
   display: inline-block;
-  width: 100%;
-  height: 100%;
-  animation: animate 2.5s linear infinite;
-  animation-delay: calc(-0.5s * var(--i));
+  width: 100px;
+  height: 100px;
+  animation: animate 2s linear infinite;
+  animation-delay: calc(-1s * var(--i));
 }
 @keyframes animate {
   0% {
     transform: scale(1);
+    opacity: 0.5;
   }
   90% {
-    transform: scale(3);
+    transform: scale(4);
   }
   100% {
-    transform: scale(4);
+    transform: scale(5);
     opacity: 0;
-  }
-}
-@keyframes opacity{
-  0%{
-    opacity: 0;
-  }
-  100%{
-    opacity: 1;
   }
 }
 </style>
